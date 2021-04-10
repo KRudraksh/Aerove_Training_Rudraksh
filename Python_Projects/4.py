2cff8ccc1f07f5444c2b5a24ce94bfa5a69c389b
KRudraksh <82200856+KRudraksh@users.noreply.github.com>
2021-04-10 16:45:35 +0530

    Update 4.py

--- Python_Projects/4.py
+++ Python_Projects/4.py
@@ -1,8 +1,8 @@
 import numpy as np
 x=np.random.normal(size=(20,20))
-y = np.array([20], dtype='int32')
+y = np.ones([20], dtype='int32')
 for i in range(20):
-    x[i]=(float(input("Element:")))
+    y[i]=(float(input("Element:")))
 
 c=x.transpose()
 z=np.matmul(c,x)

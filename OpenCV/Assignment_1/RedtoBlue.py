eccd5fc079d2d4873dd007c6fec64cde7382b683
KRudraksh <rudrakshkuchiya@gmail.com>
2021-05-03 21:52:30 +0530

    Task Completed!

--- OpenCV/Assignment_1/RedtoBlue.py
+++ OpenCV/Assignment_1/RedtoBlue.py
@@ -11,7 +11,6 @@ lower_red = np.array([30,150,50])
 upper_red = np.array([255,255,180])
 
 mask = cv2.inRange(hsv, lower_red, upper_red)
-res = cv2.bitwise_and(frame,frame, mask= mask)
 frame[mask>0]=(255,0,0)
 cv2.imshow('Converted',frame)
 cv2.waitKey(0)

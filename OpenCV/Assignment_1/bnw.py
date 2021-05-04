63be158db9061065c12d9b1f5229833baad7602c
KRudraksh <rudrakshkuchiya@gmail.com>
2021-05-04 13:22:50 +0530

    Task Completed!

--- OpenCV/Assignment_1/bnw.py
+++ OpenCV/Assignment_1/bnw.py
@@ -1,5 +1,5 @@
 import cv2
-path=r'/home/rudy-001/git_workspace/Aerove_Training_Rudraksh/OpenCV/Original.jpeg'
+path=r'/home/rudy-001/git_workspace/Aerove_Training_Rudraksh/OpenCV/Assignment_1/Original.jpeg'
 img=cv2.imread(path)
 grayimg=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY) 
 (thresh,bnw)=cv2.threshold(grayimg,127,255,cv2.THRESH_BINARY) 
